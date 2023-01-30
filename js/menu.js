ad9058096fcacbfa31dd7255540113d0385c116d
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-01-30 18:14:58 +0300

    Add checked linter errors.

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,9 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navLinks.classList.toggle('active');
-})
+});
 
-document.querySelectorAll('.nav-item').forEach((n) =>
-  n.addEventListener("click", function () {
-    hamburger.classList.remove('active');
-    navLinks.classList.remove('active');
-  })
-);
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navLinks.classList.remove('active');
+}));
