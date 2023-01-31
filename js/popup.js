78b9570ffd32560596a837f0a9990f7ba9ca4b33
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-01-31 11:42:04 +0300

    Add cards variable that store the contets of the desktop cards.

---
+++ js/popup.js
@@ -0,0 +1,55 @@
+// storing card section for the desktop part.
+const cards = [
+  {
+    title: "Tonic",
+    technology: ["Canopy", "Back End Dev", 2015],
+    images: "./images/Snapshoot-1.svg",
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://yidnekachew-cmd.github.io/Personal-Portfolio/",
+    source: "git@github.com:Yidnekachew-cmd/Personal-Portfolio.git",
+    closeBtn: "&times;",
+    languages: ["html", "css", "javaScript"],
+  },
+  {
+    title: "Multi-Post Stories",
+    technology: ["FACEBOOK", "Full Stack Dev", 2015],
+    images: "./images/Snapshoot-2.svg",
+    text: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://yidnekachew-cmd.github.io/Personal-Portfolio/",
+    source: "git@github.com:Yidnekachew-cmd/Personal-Portfolio.git",
+    closeBtn: "&times;",
+    languages: ["html", "Ruby on rails", "css", "javaScript"],
+  },
+  {
+    title: "Facebook 360",
+    technology: ["FACEBOOK", "Full Stack Dev", 2015],
+    images: "./images/Snapshoot-3.svg",
+    text: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://yidnekachew-cmd.github.io/Personal-Portfolio/",
+    source: "git@github.com:Yidnekachew-cmd/Personal-Portfolio.git",
+    closeBtn: "&times;",
+    languages: ["html", "Ruby on rails", "css", "javaScript"],
+  },
+  {
+    title: "Uber Navigation",
+    technology: ["Uber", "Lead Developer", 2018],
+    images: "./images/Snapshoot-4.svg",
+    text: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    textPopup:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus explicabo, nobis quia expedita fugit vel, est, dolorem aperiam error quibusdam itaque amet ipsa placeat numquam dolorum nemo tempore harum minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ratione corporis, neque, quae totam incidunt ut veniam estnobis sint delectus aliquid aut minima consequatur praesentium nemo impedit dolorum aspernatur.",
+    languages: ["html", "Ruby on rails", "css", "javaScript"],
+    langPopup: ["html", "css", "javaScript", "github", "ruby", "Bootstaps"],
+    live: "https://yidnekachew-cmd.github.io/Personal-Portfolio/",
+    source: "git@github.com:Yidnekachew-cmd/Personal-Portfolio.git",
+    closeBtn: "&times;",
+  },
+];
