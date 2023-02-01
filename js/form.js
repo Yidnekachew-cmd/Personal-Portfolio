326a155786bf941a8db155c5dd9733d4ed6f41dd
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-02-01 13:28:39 +0300

    Add Style for the event listner.

--- js/form.js
+++ js/form.js
@@ -1,13 +1,23 @@
-const userEmail = document.getElementById('user_email').value;
-const formBtn = document.getElementById('form-btn');
-const pattern = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
-const EMAIL_INVALID = 'Please enter a correct email address format';
 
-function emailValidator(event) {
-  if (pattern.test(userEmail)) {
+
+
+formBtn.addEventListener('click', () => {
+    console.log(email);
+});
+
+
+function emailValidator() {
+
+const email = document.getElementById('user_email').value;
+const EMAIL_INVALID = 'Please enter a correct email address format';
+const pattern = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+document.forms[0].onsubmit = function (event) {
+  if (pattern.test(email)) {
     document.getElementById('message').innerHTML = 'Success!';
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').style.fontSize = '24px';
+    document.getElementById('user_email').style.background = '#fff';
   } else {
     document.getElementById('message').innerHTML = EMAIL_INVALID;
     document.getElementById('message').style.color = '#fdd';
@@ -15,7 +25,8 @@ function emailValidator(event) {
     document.getElementById('user_email').style.border = '1px solid #900';
     document.getElementById('user_email').style.background = '#fdd';
     event.preventDefault();
+
   }
 }
+}
 
-formBtn.addEventListener('click', emailValidator);
